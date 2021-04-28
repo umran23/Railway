7f751a84ae5c9d484ca611dabdb24561603cd31a
umran23 <umran.bac@gmail.com>
2021-04-28 19:14:48 +0300

    Extends UserDetailsService

--- src/main/java/com/railway/demo/service/UserService.java
+++ src/main/java/com/railway/demo/service/UserService.java
@@ -1,8 +1,9 @@
 package com.railway.demo.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
